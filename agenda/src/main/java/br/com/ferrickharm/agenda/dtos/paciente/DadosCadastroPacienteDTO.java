7e62d1b93f928f8ba394e78f244a8c62322e8e29
Ranon Emerick <ranonemerick@gmail.com>
2024-09-06 11:32:24 -0300

    Ajustes no padrão para validação dos dados

--- agenda/src/main/java/br/com/ferrickharm/agenda/dtos/paciente/DadosCadastroPacienteDTO.java
+++ agenda/src/main/java/br/com/ferrickharm/agenda/dtos/paciente/DadosCadastroPacienteDTO.java
@@ -3,15 +3,16 @@ package br.com.ferrickharm.agenda.dtos.paciente;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 
 public record DadosCadastroPacienteDTO(@NotBlank String nomeCompleto,
-                                       @NotBlank String cpf,
+                                       @NotBlank @Pattern(regexp = "\\d{11}") String cpf,
                                        @NotNull LocalDate dataNascimento,
                                        @NotBlank String telefone,
                                        @NotBlank @Email String email,
                                        String genero,
-                                       @NotBlank String cep,
+                                       @NotBlank @Pattern(regexp = "\\d{8}") String cep,
                                        @NotBlank String endereco) {
 }
--- agenda/src/main/java/br/com/ferrickharm/agenda/dtos/profissionaldesaude/DadosCadastroProfissionalDeSaudeDTO.java
+++ agenda/src/main/java/br/com/ferrickharm/agenda/dtos/profissionaldesaude/DadosCadastroProfissionalDeSaudeDTO.java
@@ -3,17 +3,18 @@ package br.com.ferrickharm.agenda.dtos.profissionaldesaude;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 
 public record DadosCadastroProfissionalDeSaudeDTO(@NotBlank String nomeCompleto,
-                                                  @NotBlank String cpf,
+                                                  @NotBlank @Pattern(regexp = "\\d{11}") String cpf,
                                                   @NotNull LocalDate dataNascimento,
                                                   @NotBlank @Email String email,
                                                   @NotBlank String telefone,
-                                                  @NotBlank String registro,
+                                                  @NotBlank @Pattern(regexp = "\\d{4,10}") String registro,
                                                   String genero,
-                                                  @NotBlank String cep,
+                                                  @NotBlank @Pattern(regexp = "\\d{8}") String cep,
                                                   @NotBlank String endereco) {
 
 }
